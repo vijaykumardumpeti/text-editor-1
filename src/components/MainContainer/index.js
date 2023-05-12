41884f7a46bff2202b3f10a7a9be25922bb7f4a5
VIJAYKUMAR <115571763+vijaykumardumpeti@users.noreply.github.com>
2023-05-12 20:39:58 +0530

    Update index.js

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -1,97 +1,122 @@
-import {Component} from 'react'
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import {VscBold} from 'react-icons/vsc'
+import {GoItalic} from 'react-icons/go'
+import {AiOutlineUnderline} from 'react-icons/ai'
 
-import {VscBold, GoItalic, AiOutlineUnderline} from 'react-icons/all'
-
-import {
-  BgContainer,
-  Container,
-  FirstContainer,
-  SecondContainer,
-  IconsContainer,
-  Button,
-  Hr,
-  TextArea,
-  IconCon1,
-  IconCon2,
-  IconCon3,
-} from './StyledComponents'
-
-export default class MainContainer extends Component {
-  state = {
-    inputValue: '',
-    activeButton1: '',
-    activeButton2: '',
-    activeButton3: '',
-  }
-
-  onChangeInput = event => {
-    this.setState({
-      inputValue: event.target.value,
-    })
-  }
-
-  onBold = () => {
-    this.setState(prevState => ({
-      activeButton1: prevState.activeButton1 === '' ? 'bold' : '',
-    }))
-  }
-
-  onItalic = () => {
-    this.setState(prevState => ({
-      activeButton2: prevState.activeButton2 === '' ? 'italic' : '',
-    }))
-  }
-
-  onUnderline = () => {
-    this.setState(prevState => ({
-      activeButton3: prevState.activeButton3 === '' ? 'underline' : '',
-    }))
-  }
+export default class TextEditor extends Component {
+  state = {isBold: false, isItalic: false, isUnderline: false}
 
   render() {
-    const {inputValue, activeButton1, activeButton2, activeButton3} = this.state
-
+    const {isBold, isUnderline, isItalic} = this.state
     return (
-      <BgContainer>
-        <Container>
-          <FirstContainer>
-            <h1>Text Editor</h1>
-            <img
-              alt="text editor"
+      <MainContainer>
+        <EditorContainer>
+          <Container>
+            <Heading>Text Editor</Heading>
+            <Image
               src="https://assets.ccbp.in/frontend/react-js/text-editor-img.png"
+              alt="text editor"
             />
-          </FirstContainer>
-          <SecondContainer>
-            <IconsContainer>
-              <Button onClick={this.onBold} data-testid="bold">
-                <IconCon1 activeButton1={activeButton1}>
-                  <VscBold size={25} />
-                </IconCon1>
-              </Button>
-              <Button onClick={this.onItalic} data-testid="italic">
-                <IconCon2 activeButton2={activeButton2}>
-                  <GoItalic size={25} />
-                </IconCon2>
-              </Button>
-              <Button onClick={this.onUnderline} data-testid="underline">
-                <IconCon3 activeButton3={activeButton3}>
-                  <AiOutlineUnderline size={25} />
-                </IconCon3>
-              </Button>
-            </IconsContainer>
-            <div>
-              <Hr />
-            </div>
-            <TextArea
-              activeButton1={activeButton1}
-              activeButton2={activeButton2}
-              activeButton3={activeButton3}
-              value={inputValue}
-              onChange={this.onChangeInput}
+          </Container>
+          <Editor>
+            <ButtonsContainer>
+              <ButtonItems>
+                <Button
+                  data-testid="bold"
+                  onClick={() => this.setState({isBold: !isBold})}
+                  color={isBold ? '#faff00' : '#f1f5f9'}
+                >
+                  <VscBold />
+                </Button>
+              </ButtonItems>
+              <ButtonItems>
+                <Button
+                  data-testid="italic"
+                  onClick={() => this.setState({isItalic: !isItalic})}
+                  color={isItalic ? '#faff00' : '#f1f5f9'}
+                >
+                  <GoItalic />
+                </Button>
+              </ButtonItems>
+              <ButtonItems>
+                <Button
+                  data-testid="underline"
+                  onClick={() => this.setState({isUnderline: !isUnderline})}
+                  color={isUnderline ? '#faff00' : '#f1f5f9'}
+                >
+                  <AiOutlineUnderline />
+                </Button>
+              </ButtonItems>
+            </ButtonsContainer>
+            <Text
+              fontStyle={isItalic ? 'italic' : 'normal'}
+              fontWeight={isBold ? 'bold' : 'normal'}
+              textDecoration={isUnderline ? 'underline' : 'normal'}
+              row="30"
+              cols="10"
             />
-          </SecondContainer>
-        </Container>
-      </BgContainer>
+          </Editor>
+        </EditorContainer>
+      </MainContainer>
     )
   }
 }
+
+const Text = styled.textarea`
+  width: 100%;
+  height: 80%;
+  background-color: transparent;
+  color: #fff;
+  outline: none;
+  font-weight: ${props => props.fontWeight};
+  font-style: ${props => props.fontStyle};
+  text-decoration: ${props => props.textDecoration};
+`
+
+const Button = styled.button`
+  color: ${props => props.color};
+  background-color: transparent;
+  border: 0;
+`
+const ButtonItems = styled.li`
+  list-style: none;
+`
+const ButtonsContainer = styled.ul`
+  border: 1px solid #f8fafc;
+  padding: 10px;
+  display: flex;
+`
+
+const Editor = styled.div`
+  background-color: #25262c;
+  width: 100%;
+  border-radius: 30px;
+  padding: 20px;
+`
+
+const Image = styled.img`
+  width: 100%;
+`
+const Heading = styled.h1`
+  color: #fff;
+  text-align: center;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #25262c;
+  height: 100vh;
+`
+
+const EditorContainer = styled.div`
+  background-color: #1b1c22;
+  height: 80vh;
+  width: 80vw;
+  border-radius: 10px;
+  display: flex;
+  padding: 20px;
+`
+const Container = styled.div``
